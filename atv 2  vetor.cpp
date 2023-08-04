72789a1b1e1de6ca038d39cbf64464c9b5ed8c7e
jgantunes2 <125092766+jgantunes2@users.noreply.github.com>
2023-08-04 14:35:27 -0300

    Add files via upload

---
+++ atv 2  vetor.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+int vetor[10];
+int i;
+
+printf("digite um valor %d");
+scanf("%d",&i);
+
+for (i = 0 ; i + 5 ; i < 10 ; i++){
+
+printf("digite um valor %d: ");
+scanf("%d",&i);
+}
+}       
