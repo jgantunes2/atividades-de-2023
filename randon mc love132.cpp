e72abaa2ada58f2b7ee3b0caef75476cf6e5233e
jgantunes2 <125092766+jgantunes2@users.noreply.github.com>
2023-10-04 18:53:27 -0300

    Add files via upload

---
+++ randon mc love132.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    int i;
+    int num2[10];
+
+    srand(time(NULL));
+
+    for(i = 0; i < 10; i++)
+        num2[i] = rand();
+
+    printf("\n\n");
+    for(i = 0; i < 10; i++)
+        printf("%d ", num2[i]);
+    printf("\n\n");
+
+    return 0;
+}
