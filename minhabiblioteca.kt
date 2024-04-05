94df97d98d0010817d3ce422a492bf5ff07ab33c
jgantunes2 <125092766+jgantunes2@users.noreply.github.com>
2024-04-05 14:59:33 -0300

    Update minhabiblioteca.kt

--- minhabiblioteca.kt
+++ minhabiblioteca.kt
@@ -22,4 +22,41 @@ interface pesquisavel {
     fun pesquisar(palavraChave: String):List<Livro>
 }
 
-class Biblioteca(var nome: String) :
+class Biblioteca(var nome: String) : Pesquisavel {
+    private val livros: mutablelist<livro> = mutablelistof()
+
+    fun adicionarlivro(Livro: Livro) {
+        livros.add(Livro)
+        println("LIvro removido: $(livro.titulo)")
+    }
+
+    fun removerLivro(LIvro: Livro) {
+        livros.remove(livro)
+        println("Livro removido $(Livro.titulo)")
+    }
+
+    override fun pesquisar(palavraChave: String): List<Livro> {
+        return livros.filter {
+            it.titulo.contains(palavra.Chave, IgnoraCase = true) ||
+                    it.autor.contains(palavra.Chave, IgnoraCase = true)
+        }
+    }
+}
+
+fun main(){
+    val biblioteca = Biblioteca("Biblioteca Central")
+    val livro1 = Livro("Percy Jack e os olimpianos", "Rick Riordam", 2005, true)
+    val livro2 = Livro("Harry Potter", "J.K. Rowling", 1998, true)
+
+    biblioteca.adicionarLivro(livro1)
+    biblioteca.adicionarLivro(livro2)
+
+    Livro1.emprestar()
+    Livro1.devolver()
+
+    Livro2.emprestar()
+    Livro2.devolver()
+
+    val livrospesquisados = biblioteca.pesquisar("2005")
+    println("Livros encontrados: $(livrospesquisados.map(it.titulo))")
+}
